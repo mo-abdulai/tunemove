af94aa2b48883d2867ec56469200646e3d2b73e1
Mohammed Abdulai <nurud43@gmail.com>
2026-06-04 18:26:24 -0400

    Refine auth split layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
+import styles from "../../auth-layout.module.css";
 
 export default function SignInPage() {
   return (
-    <div className="w-full max-w-md">
+    <div className={styles.formFrame}>
       <SignIn path="/sign-in" signUpUrl="/sign-up" />
     </div>
   );
